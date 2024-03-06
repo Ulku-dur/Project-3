89797a172ea9858b928aeeda3fa7a4d2c35bccac
ÜlküDurmazKöker <93094752+Ulku-dur@users.noreply.github.com>
2024-03-06 20:23:23 +0300

    Update GlobalExceptionHandler.java

--- src/main/java/dev/patika/restApiVeterinarySystem/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/patika/restApiVeterinarySystem/exception/GlobalExceptionHandler.java
@@ -23,6 +23,44 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
+     @ExceptionHandler(AnimalAlreadyExistsException.class)
+    ResponseEntity<ErrorResponse> handleAnimalAlreadyExistsException (WebRequest request,
+                                                                      AnimalAlreadyExistsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatuscode(400);
+        errorResponse.setOccurenceDate(LocalDateTime.now());
+        errorResponse.setMessage(ex.getMessage());
+        // hatanın hangi url den geldiğini görmek için yazdık
+        errorResponse.setDescription(request.getDescription(false));
+        // error mesajını konsola print et
+        log.error(errorResponse.toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(AppointmentAlreadyExistsException.class)
+    ResponseEntity<ErrorResponse> handleAppointmentAlreadyExistsException (WebRequest request,
+                                                                      AppointmentAlreadyExistsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatuscode(400);
+        errorResponse.setOccurenceDate(LocalDateTime.now());
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setDescription(request.getDescription(false));
+        log.error(errorResponse.toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+     @ExceptionHandler(CustomerAlreadyExistsException.class)
+    ResponseEntity<ErrorResponse> handleCustomerAlreadyExistsException (WebRequest request,
+                                                                           CustomerAlreadyExistsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatuscode(400);
+        errorResponse.setOccurenceDate(LocalDateTime.now());
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setDescription(request.getDescription(false));
+        log.error(errorResponse.toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
     @ExceptionHandler(DoctorAlreadyExistsException.class)
     ResponseEntity<ErrorResponse> handleDoctorAlreadyExistsException (WebRequest request,
                                                                       DoctorAlreadyExistsException ex) {
@@ -33,6 +71,18 @@ public class GlobalExceptionHandler {
         // hatanın hangi url den geldiğini görmek için yazdık
         errorResponse.setDescription(request.getDescription(false));
         // error mesajını konsola print et
+        log.error(errorResponse.toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(VaccineAlreadyExistsException.class)
+    ResponseEntity<ErrorResponse> handleVaccineAlreadyExistsException (WebRequest request,
+                                                                          VaccineAlreadyExistsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatuscode(400);
+        errorResponse.setOccurenceDate(LocalDateTime.now());
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setDescription(request.getDescription(false));
         log.error(errorResponse.toString());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
